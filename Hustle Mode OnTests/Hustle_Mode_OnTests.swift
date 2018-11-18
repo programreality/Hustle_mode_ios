eb9455d2b6018888beebdf94cf45d860774a275f
Manjunath Shenoy <mshenoy98@gmail.com>
2018-11-18 19:51:12 +0530

    Initial Commit

---
+++ Hustle Mode OnTests/Hustle_Mode_OnTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hustle_Mode_OnTests.swift
+//  Hustle Mode OnTests
+//
+//  Created by Manjunath Shenoy on 18/11/18.
+//  Copyright © 2018 Manjunath Shenoy. All rights reserved.
+//
+
+import XCTest
+@testable import Hustle_Mode_On
+
+class Hustle_Mode_OnTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
